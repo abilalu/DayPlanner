f4618505b18581f72972ef82ea2bdcf24c192279
abi <abilalu1994@gmail.com>
2022-01-28 08:34:17 -0600

    Adding files to the repository created

---
+++ Develop/JS/script.js
@@ -0,0 +1,61 @@
+
+$( document ).ready(function() {
+var a= moment().format('dddd MMMM Do YYYY, h:mm');
+  $("#display-date").text(a)
+  var row = ""
+  
+    for (var i= 6 ; i<=18; i++){
+      row = $(`<div class="row">`)
+      col1 = $(`<div class ="col-lg-2 hour">${displayAmorPm(i)}</div>`)
+      col2 = $(`<div class ="col-lg-8 inputcontent"><input data-input="${i}" id="inputText${i}" class="form-control inputText" type="text" name="userInput"></div>`)
+      col3 = $(`<div class ="col-lg-2"><button data-id="${i}" id="savePlanner" class="btn btn-success btn-block"><i class="fas fa-save"></i> Create Event</button></div>`)
+      row.append(col1)
+      row.append(col2)
+      row.append(col3)
+      $("#display-planner").append(row)
+      getlocalStorage(i)
+    }
+   $("button.btn.btn-success").click(function(e){
+   var id = $(this).data("id")
+   var inputText = $(this).parent().siblings().find("input").val()
+   localStorage.setItem(id,inputText)
+   })
+  //   Am to Pm time zone
+   function displayAmorPm(hour){
+     var b=""
+     if(hour<=12){
+       b= "AM"
+     }else{
+       b="PM"
+     }
+     hour = hour % 12
+     hour = hour ? hour : 12
+     return hour + " " + b
+   }
+  });
+
+   function getlocalStorage(hour){
+     let inputval = localStorage.getItem(hour)
+     if(true){
+      
+      var text= $(`input#inputText${hour}`).val(inputval)
+      console.log(text)
+     }
+   }
+   //Update the color
+   function updateColor(){
+     var hour = new Date().getHours();
+     for (var i= 6 ; i<=18 ;i++){
+       console.log(hour,i)
+       if(hour==i ) {
+        $(`#inputText${i}`).css("background","orange")
+       }else  if(hour<i ){
+        
+         $(`#inputText${i}`).css("background","lightyellow")
+
+       }
+     }
+   }
+   setInterval(function(){
+     updateColor()
+   },1000)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DayPlanner
+This day planner is created to schedule the events of a day from 6 AM to 6 PM. When we open the page current time and date is diplayed. Events can be created next to the time displayed below. Current event is displayed on Orange tab.
+The following image shows the web application's appearance and functionality:
+![When you load the page you will see like this](sc1.png)
+![When you create events, you will see this](sc2.png)
